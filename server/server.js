0b06abbbf525c4a2d78fd23f3c38a5402d788fc8
bmichare <65011771+bmichare@users.noreply.github.com>
2022-03-05 13:59:57 -0600

    Revert "Louie mjr/micjun backend"

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const express = require('express');
-const router = require('./routers/routes');
 
 const app = express();
 const PORT = 3000;
@@ -8,11 +7,11 @@ const PORT = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
 app.get('/', (req, res) => {
   res.send('!!!!!IS THIS WORKING!!!!!');
 });
 
-app.use('/login', router);
 
 app.listen(PORT, () => {
   console.log(`Server Listening on port: http://localhost:${PORT}`);
