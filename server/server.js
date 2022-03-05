9219c78c1647dc553c1a3038e507f8e847179b90
micjun <jung.michael@protonmail.com>
2022-03-05 15:22:58 -0500

    server

--- server/server.js
+++ server/server.js
@@ -1,10 +1,7 @@
 const path = require('path');
 const express = require('express');
-<<<<<<< HEAD
 const mongoose = require('mongoose');
 const loginRouter = require('./routers/loginRouter');
-=======
->>>>>>> 25b7a7df96da627623f468d3c7e9d11015db22ef
 
 const app = express();
 const PORT = 3000;
@@ -22,10 +19,7 @@ app.get('/', (req, res) => {
   res.send('!!!!!IS THIS WORKING!!!!!');
 });
 
-<<<<<<< HEAD
 app.use('/login', loginRouter);
-=======
->>>>>>> 25b7a7df96da627623f468d3c7e9d11015db22ef
 
 app.listen(PORT, () => {
   console.log(`Server Listening on port: http://localhost:${PORT}`);
