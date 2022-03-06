7b22bd62e99dab7819bb2a489ca5755536f3cb79
Louie Mendez <lmendezjr024@gmail.com>
2022-03-06 16:14:09 -0500

    added google Oauth/passportjs

--- server/server.js
+++ server/server.js
@@ -2,14 +2,14 @@ const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
 const loginRouter = require('./routers/loginRouter');
-
+const googleRouter = require('./routers/googleRouter');
 const app = express();
 const PORT = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// mongodb+srv://lmendezjr024:<password>@cluster0.npof1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+
 
 mongoose.connect('mongodb+srv://scratch-project:dg8PH0oE2bAwgKEr@cluster0.npof1.mongodb.net/scratch-projectDB?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connection.once('open', () => {
@@ -19,6 +19,7 @@ mongoose.connection.once('open', () => {
 app.use("/", express.static(path.join(__dirname, "../dist")));
 
 app.use('/data/login', loginRouter);
+app.use('/data/oauth', googleRouter);
 
 app.listen(PORT, () => {
   console.log(`Server Listening on port: http://localhost:${PORT}`);
